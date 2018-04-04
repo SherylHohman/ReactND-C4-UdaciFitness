38f597802b3237fbac2abcfffc6dad6e0af8483f
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-04 04:35:46 -0700

    feat: add state and state methods to AddEntry
      On branch sh-1
    	- modified:   components/AddEntry.js

--- UdaciFitness/components/AddEntry.js
+++ UdaciFitness/components/AddEntry.js
@@ -3,6 +3,43 @@ import { View, Text } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 export default class AddEntry extends Component {
+
+  state = {
+    bike:  0,
+    run:   0,
+    swim:  0,
+    sleep: 0,
+    eat:   0,
+}
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((state) => {
+      const newValue = state[metric] + step;
+      return ({
+        ...state,
+        [metric]: (newValue>max) ? max : newValue,
+      });
+    });
+  }
+
+  decrement = (metric) => {
+    const { step } = getMetricMetaInfo(metric);
+
+    this.setState((state) => {
+      const newValue = state[metric] - step;
+      return ({
+        ...state,
+        [metric]: (newValue<0) ? 0 : newValue,
+      });
+    });
+  }
+
+  slide = (metric, value) => {
+    this.setState({ [metric]: value })
+  }
+
   render(){
     return (
       <View>
