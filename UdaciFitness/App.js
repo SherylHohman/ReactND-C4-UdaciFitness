965828595e406304f9b09ed27ef95925856ecd9f
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-05 08:59:05 -0700

    feat: enable scrolling
      In App.js, wrappting the root <View> in <ScrollView>
        enables scrolling within the app, when content does not
        fully fit on the screen.
    
      Not part of the course version of the app.  But my content did
        not fit on the app screen, and I could not scroll to see relevant
        changes!  Fixed That !
    
    	 - modified:   ../App.js

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
+      <ScrollView>
+        <View>
+          <AddEntry />
+        </View>
+      </ScrollView>
     );
   }
 }
