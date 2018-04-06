1d87397e046ce09e103e4d41d6c31b19e0cb2df2
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-06 13:31:47 -0700

    feat: add styles: App, and getMetricMetaInfo:icons
    	modified:   ../App.js
    	modified:   ../utils/helpers.js

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -6,12 +6,16 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import AddEntry from './components/AddEntry';
 
+// temp: disable showing react-native deprecation warnings in emulator
+import {YellowBox} from 'react-native';
+console.disableYellowBox = true;
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <ScrollView>
-          <View>
+          <View style={{flex: 1}}>
             <AddEntry />
           </View>
         </ScrollView>
--- UdaciFitness/utils/helpers.js
+++ UdaciFitness/utils/helpers.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { white } from './colors'
+import { white, red, orange, blue, lightPurp, pink } from './colors'
 
 export function getMetricMetaInfo (metric) {
   const info = {
@@ -13,11 +13,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
-          {/*<View style={[styles.iconContainer, {backgroundColor: red}]}>*/}
+          <View style={[styles.iconContainer, {backgroundColor: red}]}>
             <MaterialIcons
               name='directions-run'
-              color={'black'}
+              color={white}
               size={35}
             />
           </View>
@@ -32,11 +31,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
-          {/*<View style={[styles.iconContainer, {backgroundColor: orange}]}>*/}
+          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
             <MaterialCommunityIcons
               name='bike'
-              color={'black'}
+              color={white}
               size={32}
             />
           </View>
@@ -51,11 +49,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
-          {/*<View style={[styles.iconContainer, {backgroundColor: blue}]}>*/}
+          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
             <MaterialCommunityIcons
               name='swim'
-              color={'black'}
+              color={white}
               size={35}
             />
           </View>
@@ -70,11 +67,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
-          {/*<View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>*/}
+          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
             <FontAwesome
               name='bed'
-              color={'black'}
+              color={white}
               size={30}
             />
           </View>
@@ -89,11 +85,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
-          {/*<View style={[styles.iconContainer, {backgroundColor: pink}]}>*/}
+          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
             <MaterialCommunityIcons
               name='food'
-              color={'black'}
+              color={white}
               size={35}
             />
           </View>
@@ -106,6 +101,18 @@ export function getMetricMetaInfo (metric) {
     ? info
     : info[metric]
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width:  50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems:     'center',
+    marginRight: 20,
+  },
+});
 
 export function isBetween (num, x, y) {
   if (num >= x && num <= y) {
