de63e1689a1b52c725c96b4c0fda3283bd223e71
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-05 17:44:30 -0700

    feat: add action creators, reducer (prep for redux)
      ----
    	new file:   ../actions/index.js
    	new file:   ../reducers/index.js

---
+++ UdaciFitness/reducers/index.js
@@ -0,0 +1,23 @@
+import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions';
+
+export function = entries(state={}, action) => {
+  switch (action.type) {
+
+    case RECEIVE_ENTRIES :
+      return ({
+        ...state,
+        ...action.entries,
+      });
+
+    case ADD_ENTRY :
+      return ({
+        ...state,
+        ...action.entry,
+        // using spread on "entry" not required; future proofing in case add an additional key to the "entry"
+      });
+
+    default return state;
+  }
+}
+
+export default entries;
