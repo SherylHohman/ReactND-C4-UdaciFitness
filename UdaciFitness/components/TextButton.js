5fd44e598c91c2b75648157dfb13c53d96cb08f9
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-07 12:52:03 -0700

    feat: style the reset button for "alreadyLogged"
    	modified:   TextButton.js

--- UdaciFitness/components/TextButton.js
+++ UdaciFitness/components/TextButton.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { purple } from '../utils/colors';
 
 export default function TextButton({ children, onPress, btnStyle={}, txtStyle={} }) {
   return (
       <TouchableOpacity onPress={onPress} style={btnStyle}>
-        <Text style={txtStyle}>{children}</Text>
+        <Text style={[styles.reset, txtStyle]}>{children}</Text>
       </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: 'center',
+    color: purple,
+  }
+})
