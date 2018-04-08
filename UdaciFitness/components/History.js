157f97250b2faaa7fdc1f3988205374d0c100b3e
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-07 18:27:16 -0700

    feat: integrate udacifitenss-calendar to  History
      History component now renders data, based on user selected date
        from calendar.
    
      Displays JSON.stringify of fetched data.
        shows data starting from the day selected, all the way to the present day
    
      TODO: Issue - ios works as expected, but..
        Android always shows all data it has received.
    
        For example,
        - I select today, it shows proper data for
          today
        - Then I select yesterday, and it properly shows data for
          yesterday and today
        - Now I re-select today.
          Expected: show data for today only (from today to present aka today)
          Actual:  shows data for today AND Yesterday !  Yesterday should not be here.
    
          Do Not know if it is an known issue with udacifitenss-calendar
          or if somehow my app is preventing proper fetch/store/display of data
          given the nature of Android vs IOS specific code, I do not know how my
          app could have mis-coded something to yield this type of result for
          for Android only..
          Then again, I would expect that if there was an issue with their
          component, we would have been warned..
    
    	modified:   History.js

--- UdaciFitness/components/History.js
+++ UdaciFitness/components/History.js
@@ -3,6 +3,7 @@ import React , { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Ionicons } from '@expo/vector-icons';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
 // api
 import { fetchCalendarResults } from '../utils/api';
 // Actions
@@ -13,6 +14,9 @@ import { purple, white } from '../utils/colors'
 
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
 
   componentDidMount(){
     const { dispatch } = this.props;
@@ -29,16 +33,59 @@ class History extends Component {
           }));
         }
       })
+      .then(() => this.setState(() => ({ready: true})));
+  }
+
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+      <View>
+        {today
+          ? <Text>{JSON.stringify(today)}  </Text>
+          : <Text>{JSON.stringify(metrics)}</Text>
+        }
+      </View>
+    )
+  renderEmptyDate({ formattedDate }){
+    // if store has "today" value (the generic message to remember to log data today)
+    // this value is not saved to "DB"/Async
+    // it is only on the current date, and only in the store.
+    return (
+      <View>
+       <Text>No Data for this Date</Text>
+      </View>
+    )
   }
 
   render(){
+    const { entries } = this.props;
+
+    if (!this.state.ready){
+      return (
+        <View><Text>fetching you data..</Text></View>
+      );
+    }
+
     return (
-      <View>
-        <Text>{JSON.stringify(this.props)}</Text>
-      </View>
+      // TODO: Android shows all prev fetched data, even when
+      //  selected date has changed to a later date.
+      //  It *should*  only show data from selected date to present.
+      //  ios is working as expected.
+      //    eg: select 7th (today) shows today.
+      //    now select 6th (yesterday). shows yesterday and today
+      //    now select 7th again. Should just show today,
+      //                          but still shows yesterday's data also.
+      // Is this a known issue with the udacifitness-calendar api ?
+      // or is my app preventing proper display, fetch, or store of data ?
+
+      <UdaciFitnessCalendar
+        // calendar takes 2 callbacks. It checks our (items) data, then calls
+        // the appropriate callback. If the data for that date is null or not.
+        // It will render all info from the selected date to the present day.
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     );
   }
-
 }
 
 function mapStateToProps(store){
