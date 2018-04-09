8fb467fe7b5d4af905acb1e20e374c544e22b034
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-09 16:25:54 -0700

    feat: History Component cards link to EntryDetail page
      When a MetricCard
        (rendered for dates that have saved workout/metric data)
        is clicked, the App
        (via access to a StackNavigator navigation.navigate() method)
          (in MainNavigation)
        navigates to EntryDetail page/component
          ('EntryDetail' is one of the keys defined on MainNavigation)
          (EntryDetail component is a screen defined for the 'EntryDetail' key)
    
      The MetricCard passes along the date of the card that was clicked On
        it is passed as:   `entryId={entryId}`
        it is received as: `this.props.navigation.params.entryId`
    
      EntryDetail now has what it needs to render additional information
        for the chosen date.
    
      TODO: turn skeleton EntryDetail into a fleshed out component
    
    	- modified:   components/EntryDetail.js
    	- modified:   components/History.js

--- UdaciFitness/components/EntryDetail.js
+++ UdaciFitness/components/EntryDetail.js
@@ -2,13 +2,26 @@ import React , { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class EntryDetail extends Component{
+
   render () {
-    <View>
-      <Text>
-        Entry Detail Page
-      </Text>
-    </View>
-  }
-};
+
+    // this.pros.navigation is passed in via StackNavigator (Main Navigation component)
+    // ...state.params.entryId is passed in from the component that requested
+    // navigation to this page when it called navigation.navigate('EntryDetail')
+    //   any user defined props passed in when calling a StackNavigator
+    //   navigate() method will be found on navigation's
+    //   "params" property.
+    const entryId = this.props.navigation.state.params.entryId;
+
+    return (
+      <View>
+        <Text>
+          Entry Detail Page for: {'\n'}
+          {entryId}
+        </Text>
+      </View>
+    )
+  };
+}
 
 export default EntryDetail
