a9092cc62666013878c7c10cd2587dc2068df77f
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-09 16:59:26 -0700

    feat: dynamically set title in Page Header
      StackNavigator can render a page title via its
        [routeKey]: navigationOptions{ title } property
        (Not sure what the official term is for the `routeKey` quoted above)
    
      Defined a `static` method on the EntryDetail page/component/route
        called `navigationOptions`:
          1) props are automatically passed in when the component (loads/mounts??)
          2) `navigation` is automatically passed into this component as a props
             (passed in by StackNavigator)
          3) `navigationOptions` is a property for this StackNavigator "route"
              I can *dynamically* add the a `title` property to this instance of
              `navigationOptions`, using another property that was passed into the
              component as its value.
              I'm not sure how static works, but it *seems* that this merges
              the `navigationOptions` defined for this component in App.js
              with the navigationOptions I return from this method. (is it a method?)
          4) StackNavigator renders whatever value is in that `title` property
              in the StackNavigator `header` (for this component/instance)
          5) End Result: The date for this metric's Entry is displayed at the top
             of the page
    
    	- modified:   components/EntryDetail.js

--- UdaciFitness/components/EntryDetail.js
+++ UdaciFitness/components/EntryDetail.js
@@ -3,6 +3,29 @@ import { View, Text } from 'react-native';
 
 class EntryDetail extends Component{
 
+  // this takes the navigationOptions, defined in App.MainNavigation
+  // (created by params we passed into StackNavigator)
+  // and *dynamically* adds a property to it.
+  // this property, is `title`.
+  // `navigationOptions.title` is a property defined by StackNavigator
+  //  is a property that StackNavigator knows how to render
+  //  (so basically we are overwriting its defalute value of: {title: ''})
+  // ** Dynamically ** using data passed into this component
+
+  // navigation is being pulled from this.props
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
+
+    const year  = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day   = entryId.slice(8);
+
+    // TODO: I'd like to also show the day of the week
+    // actually I'd prefer this style: Mon, 9 Apr 2018
+
+    return { title: `${month}/${day}/${year}`}
+  }
+
   render () {
 
     // this.pros.navigation is passed in via StackNavigator (Main Navigation component)
