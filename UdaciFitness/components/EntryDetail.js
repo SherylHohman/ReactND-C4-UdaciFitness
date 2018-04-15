1d7d27cb28d53706ab6c15965b69de668092344b
Sheryl Hohman <sherylhohman@yahoo.com>
2018-04-14 17:34:38 -0700

    style: edit/move EntryDetail comments and notes
    	modified:   components/EntryDetail.js

--- UdaciFitness/components/EntryDetail.js
+++ UdaciFitness/components/EntryDetail.js
@@ -13,15 +13,10 @@ import { white } from '../utils/colors';
 
 class EntryDetail extends Component{
 
-  // this takes the navigationOptions, defined in App.MainNavigation
-  // (created by params we passed into StackNavigator)
-  // and *dynamically* adds a property to it.
-  // this property, is `title`.
-  // `navigationOptions.title` is a property defined by StackNavigator
-  //  is a property that StackNavigator knows how to render
-  //  (so basically we are overwriting its defalute value of: {title: ''})
-  // ** Dynamically ** using data passed into this component
-
+  // STATIC
+  // ** Dynamically ** adds a property to StackNavigator option:
+  // `navigationOptions`
+  // using data passed into this component
   // navigation is being pulled from this.props
   static navigationOptions = ({ navigation }) => {
     const { entryId } = navigation.state.params;
@@ -37,14 +32,6 @@ class EntryDetail extends Component{
   }
 
   shouldComponentUpdate(nextProps){
-    // When reset button is pressed, this.props.metrics changes.
-    //   Then we navigate back to the History Page.
-    //  But, before we actually navigate, the component will try to
-    //  re-render.  If it does so when
-    //  metrics===null OR only has a today property,
-    //  then MetricCard will break, as it can only render actual metric data.
-    // Thus, we tell the component to only update if
-    // we have actual metric information for the day in question
     return (nextProps.entryId && nextProps.metrics && !nextProps.metrics.today);
   }
 
@@ -56,11 +43,6 @@ class EntryDetail extends Component{
       key: entryId,
     });
 
-    // store gets a slightly nuanced version, compared to the DB.
-    // if date is !today,
-    //  store gets the value of: null, but does not delete the item (the DB does delete the item)
-    // and if the entryId date *is* today,
-    //  store instead gets a key called "today" whose value is a "reminder" message
     const value = (entryId === timeToString())
       ? getDailyReminderValue()
       : null
@@ -108,9 +90,6 @@ function mapDispatchToProps(dispatch, { navigation }){
 }
 
 function mapStoreToProps(store, { navigation }){
-  // navigation is a prop automatically passed in by StackNavigator
-  //  props I pass in via the call to navigation.navigate(),
-  //  get placed inside `props.navigation.state.params` by StackNavigator
   const { entryId } = navigation.state.params;
 
   // metrics data saved for this date
@@ -124,3 +103,47 @@ function mapStoreToProps(store, { navigation }){
 }
 
 export default connect(mapStoreToProps, mapDispatchToProps)(EntryDetail)
+
+
+// NOTES
+  //mapStoreToProps, metrics:
+    // if date for this date is "today" reminder, or "undefined"/null
+    //   this page is unreachable.
+    // History card for those cases calls AddEntry instead.
+    // The ONLY link to EntryDetail comes from a History "card" WITH METRIC DATA
+    //   If that changes, this function may need updated.
+
+  // navigation
+    // navigation is a prop automatically passed in by StackNavigator
+    //  props I pass in via the call to navigation.navigate(),
+    //  get placed inside `props.navigation.state.params` by StackNavigator
+
+  // updateStore
+    // store gets a slightly nuanced "reset", compared to the DB.
+    // if date is !today,
+    //  store gets the value of: null,
+    //  but does not delete the item (the DB does delete the item)
+    // if the entryId date *is* today,
+    //  store instead gets a key called "today" whose value is a "reminder" message
+
+  // shouldComponentUpdate
+    // When reset button is pressed, this.props.metrics changes.
+    //   Then we navigate back to the History Page.
+    //  But, before we actually navigate, the component will try to
+    //  re-render.  If it does so when
+    //  metrics===null OR only has a today property,
+    //  then MetricCard will break, as it can only render actual metric data.
+    // Thus, we tell the component to only update if
+    // we have actual metric information for the day in question
+
+  // STATIC navigationOptions
+    // - navigation is being pulled from this.props
+    // - STATIC
+      // takes the navigationOptions, defined in App.MainNavigation
+      // (created by params we passed into StackNavigator)
+      // and *dynamically* adds a property to it.
+      // this property, is `title`.
+      // `navigationOptions.title` is a property defined by StackNavigator
+      //  is a property that StackNavigator knows how to render
+      //  (so basically we are overwriting its defalute value of: {title: ''})
+      // ** Dynamically ** using data passed into this component
